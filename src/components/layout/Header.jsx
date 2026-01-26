a9faa620754cb2bfc0a64d66e044e659480ba696
pareesa969 <pareesakhan613@gmail.com>
2026-01-26 22:05:44 +0500

    feat(layout): add application header component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="d-flex justify-content-between align-items-center">
+      <h4 className="m-0">Mini Skill Learning App</h4>
+    </div>
+  );
+}
