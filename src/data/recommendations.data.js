c3362e3178ee18c5cdd4c1e343f591d4c03ce128
pareesa969 <pareesakhan613@gmail.com>
2026-02-04 06:30:16 +0500

    feat(data): add questionnaire structure and recommendation mappings

---
+++ src/data/recommendations.data.js
@@ -0,0 +1,35 @@
+export const SKILL_RESOURCES = {
+  "Emotional Regulation": [
+    {
+      title: "Calming Strategies for Children",
+      type: "Article",
+      link: "https://www.understood.org/",
+    },
+    {
+      title: "Breathing Exercises",
+      type: "Activity",
+      link: "https://www.gozen.com/",
+    },
+  ],
+  "Homework Support": [
+    {
+      title: "Homework Routines That Work",
+      type: "Guide",
+      link: "https://childmind.org/",
+    },
+  ],
+  "Sensory Management": [
+    {
+      title: "Understanding Sensory Overload",
+      type: "Video",
+      link: "https://www.youtube.com/",
+    },
+  ],
+  "Communication Skills": [
+    {
+      title: "Improving Child Communication",
+      type: "Article",
+      link: "https://www.speechpathology.com/",
+    },
+  ],
+};
